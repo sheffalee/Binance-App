f6c7f282cdb04ac1e1f622ba5f75ec0645cf2ffa
Sheffalee <87572002+sheffalee@users.noreply.github.com>
2023-11-28 14:44:54 +0530

    Update app_refactored.py

--- app_refactored.py
+++ app_refactored.py
@@ -52,7 +52,7 @@ for i in range(len(crpytoList.keys())):
 st.header('**All Price**')
 st.dataframe(df)
 
-st.info('Credit: Created by Chanin Nantasenamat (aka [Data Professor](https://youtube.com/dataprofessor/))')
+# st.info('Credit: Created by Chanin Nantasenamat (aka [Data Professor](https://youtube.com/dataprofessor/))')
 
 st.markdown("""
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
